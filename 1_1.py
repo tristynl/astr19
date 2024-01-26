bda40c4d70690249693f4d1e578f877e1c5a037c
tristynl <tristyn-lai@outlook.com>
2024-01-25 17:45:04 -0800

    Session 1 Prompt for Coding Journal 1

---
+++ 1_1.py
@@ -0,0 +1,10 @@
+#Comment
+
+def main():
+    print("Tristyn Lai")
+    print("Pronouns: She/Hers")
+    print("My favorite movie is Bohemian Rhapsody.\nMy favorite food is noodles.")
+
+
+if __name__=="__main__":
+    main()

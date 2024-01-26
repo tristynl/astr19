f8c9125da6ba1c143310043845d3f109297b4081
tristynl <tristyn-lai@outlook.com>
2024-01-25 17:50:49 -0800

    Session 2 Prompt for Coding Journal 2

---
+++ 1_2.py
@@ -0,0 +1,20 @@
+
+def main():
+    t = 3.0
+    s = 5.0
+    l = 8
+    z = 1
+
+    sum = t + s
+    difference = l - z
+    multiply = t * l
+    print("Sum: {}".format(sum))
+    print(type(sum))
+    print("Difference: {}".format(difference))
+    print(type(difference))
+    print("Muliplay: {}".format(multiply))
+    print(type(multiply))
+
+
+if __name__=="__main__":
+    main()
